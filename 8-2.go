039228c7ff254e10873b79cea814f6d3deebd0ec
Will Luer <will.luer@gmail.com>
2023-12-09 16:57:16 -0500

    8 pt 2 with lcm

--- 8-2.go
+++ 8-2.go
@@ -14,35 +14,6 @@ type Node struct {
 	right string
 }
 
-// greatest common divisor (GCD) via Euclidean algorithm
-func GCD(a, b int) int {
-	for b != 0 {
-		t := b
-		b = a % b
-		a = t
-	}
-	return a
-}
-
-func LCM(a, b int, integers ...int) int {
-	result := a * b / GCD(a, b)
-
-	for i := 0; i < len(integers); i++ {
-		result = LCM(result, integers[i])
-	}
-
-	return result
-}
-
-func isFinished(nodes []string) bool {
-	for _, n := range nodes {
-		if !strings.HasSuffix(n, "Z") {
-			return false
-		}
-	}
-	fmt.Println("Returning true for ", nodes)
-	return true
-}
 func main() {
 	filename := "8.txt"
 	file, err := os.Open(filename)
@@ -75,36 +46,25 @@ func main() {
 		i++
 	}
 	currentNodes := startingNodes
-	count := 0
 
-	// for !isFinished(currentNodes) {
-	// 	count += 1
-	// 	for j, currentNode := range currentNodes {
-	// 		currentTurn := directions[(count-1)%len(directions)]
-	// 		if currentTurn == "L" {
-	// 			currentNodes[j] = nodeMap[currentNode].left
-	// 		}
-	// 		if currentTurn == "R" {
-	// 			currentNodes[j] = nodeMap[currentNode].right
-	// 		}
-	// 	}
-	// }
-
-	for currentNode != endingNode {
-		fmt.Println("Current node ", currentNode)
-		count += 1
-		currentTurn := directions[i%len(directions)]
-		fmt.Println(currentTurn)
-		if currentTurn == "L" {
-			currentNode = nodeMap[currentNode].left
+	var counts []int
+	for _, currentNode := range currentNodes {
+		count := 0
+		for !strings.HasSuffix(currentNode, "Z") {
+			count += 1
+			currentTurn := directions[(count-1)%len(directions)]
+			if currentTurn == "L" {
+				currentNode = nodeMap[currentNode].left
+			}
+			if currentTurn == "R" {
+				currentNode = nodeMap[currentNode].right
+			}
+			i++
 		}
-		if currentTurn == "R" {
-			currentNode = nodeMap[currentNode].right
-		}
-		i++
+		counts = append(counts, count)
 	}
+	fmt.Println("counts ", counts)
 
-	fmt.Println("Steps ", count)
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
